8387fad7322b99caa6e95f71945aba6fa457705f
Justin Hileman <justin@justinhileman.info>
2016-03-06 13:05:19 -0800

    Initialize immediately, rather than waiting for an event.

--- init.fish
+++ init.fish
@@ -1,6 +1,4 @@
-function init -a path --on-event init_vundle
-  if [ ! -e ~/.vim/bundle/Vundle.vim/.git ]
-    echo \n\t "Vundle.vim is not installed."
-    echo \n\t "Read about vim configuration for vundle at https://github.com/VundleVim/Vundle.Vim" \n
-  end
+if [ ! -e ~/.vim/bundle/Vundle.vim/.git ]
+  echo \n\t "Vundle.vim is not installed."
+  echo \n\t "Read about vim configuration for vundle at https://github.com/VundleVim/Vundle.Vim" \n
 end
